0275329c97458c25220550fe25b6967e7ac4a2f4
carlos9310 <15356059310@163.com>
2019-10-13 19:47:44 +0800

    fix UnboundLocalError

--- dialogue-system/processMessage.py
+++ dialogue-system/processMessage.py
@@ -24,7 +24,7 @@ def processMessage(sentence):
                     dm.slot["checkWeather"]["city"] = city
                 if time:
                     dm.slot["checkWeather"]["time"] = time
-                    currentEvent = "checkWeather"
+                currentEvent = "checkWeather"
             break
         elif event == "takeTaxi":
             if entities:
@@ -98,13 +98,15 @@ def processMessage(sentence):
             return "主人，请您告诉小安您要查询%s哪个城市的天气？"%dm.slot["checkWeather"]["time"]
         if needTime == 0 and needCity == 0:
             dm.processing.remove("checkWeather")
-            return "主人，小安为您要查询到%s%s的天气为晴朗，微风阵阵"%(dm.slot["checkWeather"]["city"],dm.slot["checkWeather"]["time"])
+            dm.slot["checkWeather"]["city"],  dm.slot["checkWeather"]["time"] = '', ''
+            return "主人，小安为您要查询到%s%s的天气为晴朗，微风阵阵"%(city,time)
     elif currentEvent ==  "takeTaxi":
         if not dm.slot["takeTaxi"]["destination"]:
             return "主人，请告诉小安您要去哪里？"
         else:
             dm.processing.remove("takeTaxi")
-            return "主人，小安已经为您预定好去%s的车，请您做好准备" %(dm.slot["takeTaxi"]["destination"])
+            dm.slot["takeTaxi"]["destination"] = ''
+            return "主人，小安已经为您预定好去%s的车，请您做好准备" %(place)
     else:
         #print("主人，小安没有听懂您的意思，我会继续好好学习的。")
         return "主人，小安没有听懂您的意思，我会继续好好学习的。小安可以帮您查天气，打车。"
